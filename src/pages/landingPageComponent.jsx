3da2440111bccef45604a731ee5a2af9bc8d2d8f
barbara90 <olah.barbara.1990@gmail.com>
2023-12-17 13:04:12 +0100

    Added Profile Picture

--- src/pages/landingPageComponent.jsx
+++ src/pages/landingPageComponent.jsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 export function LandingPage() {
     return(
         <div>
+        
             <h1> Barbara Oláh </h1>
+            <picture>
+                <source srcSet="src/source/BO_CV1.jpg" media="(orientation: portrait)"/>
+            </picture>
             <article> I work as a Project Coordinator, but I am passionate about other segments of the IT too. I have experience in multiple roles since 2016 like
                  <b> Software Quality Engineering</b> and <b>Fullstack Software Engineering</b> as well. Here you can find my LinkedIn profile, please check it and let's connect! 
             <nav> <a href="https://www.linkedin.com/in/barbara-ol%C3%A1h-64316960/">LinkedIn</a></nav>
