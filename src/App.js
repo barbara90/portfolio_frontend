6a20183629f01ffd245b53dd4de9b56b40419dff
barbara90 <olah.barbara.1990@gmail.com>
2023-12-17 14:06:03 +0100

    Added contact form componens to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {LandingPage} from './pages/landingPageComponent'
+import {LandingPage} from './pages/landingPageComponent';
+import {ContactForm} from './pages/contactPageComponent';
 
 function App() {
   return (
     <div className="App">
       <LandingPage/>
+      <ContactForm/>
     </div>
   );
 }
